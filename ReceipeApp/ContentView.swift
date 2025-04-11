0ea52d760494f9f6614f8874b423cddb81db2fec
saibharadwaj135 <saibharadwaj135@gmail.com>
2025-04-11 16:37:49 -0400

    Initial Commit

---
+++ ReceipeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReceipeApp
+//
+//  Created by Sai bharadwaj Adapa on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReceipeApp/ReceipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipeAppApp.swift
+//  ReceipeApp
+//
+//  Created by Sai bharadwaj Adapa on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ReceipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
